05f5a64390781c8e5a7462b077aad7eedbc18734
Gage <gage.bonner@gmail.com>
2024-10-04 18:11:04 -0500

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ makedocs(
     format = MarkdownVitepress(
         # md_output_path = ".", # LOCAL ONLY
         # build_vitepress = false, # LOCAL ONLY
-        repo = "github.com/70Gage70/Sargassum.jl.git",
+        repo = "github.com/70Gage70/Sargassum.jl",
         devbranch = "master", 
         devurl = "dev", 
     ),
@@ -45,7 +45,7 @@ makedocs(
 # )
 
 deploydocs(;
-    repo = "github.com/70Gage70/Sargassum.jl.git",
+    repo = "github.com/70Gage70/Sargassum.jl",
     target = "build", 
     versions = nothing,
     branch = "gh-pages",
